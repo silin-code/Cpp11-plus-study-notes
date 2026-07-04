8d67afab24edc8e79d0d2a50e2cc286bbe0fc005
unknown <2921098172@qq.com>
2026-07-04 21:50:37 +0800

    使用find_package引用本地静态库

---
+++ CMake/CMake3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "my_math.h"
+
+int main()
+{
+    std::cout << "10 + 20 = " << add(10, 20) << std::endl;
+    std::cout << "10 - 20 = " << sub(10, 20) << std::endl;
+    return 0;
+}
