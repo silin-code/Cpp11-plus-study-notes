99fb30222b921f9e54ed6d404a2cc3ef06e6d5a4
silin-code <2921098172@qq.com>
2026-08-28 18:47:52 +0800

    CMake的CPack打包

---
+++ CMake/CMake10-cpack_MyMath/app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "my_math.h"
+
+int main()
+{
+    std::cout << "3 + 4 = " << add(3, 4) << std::endl;
+    std::cout << "3 - 4 = " << sub(3, 4) << std::endl;
+    return 0;
+}
